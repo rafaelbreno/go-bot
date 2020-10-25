e8dbeae834fdb826309ac293b53cf2474b6cd02e
rafaelbreno <rafael_breno@outlook.com>
2020-10-25 19:39:46 -0300

    Refactor project structure

--- main.go
+++ main.go
@@ -1,64 +1,45 @@
 package main
 
 import (
-	"context"
+	"bufio"
 	"fmt"
-	"log"
-	"os"
+	"github.com/rafaelbreno/go-bot/cmd/connection"
+	"github.com/rafaelbreno/go-bot/parser"
 
-	"github.com/joho/godotenv"
-	"golang.org/x/oauth2/clientcredentials"
-	"golang.org/x/oauth2/twitch"
+	"net"
+	"net/textproto"
 )
 
-type Credentials struct {
-	ClientID     string
-	ClientSecret string
-	TokenURL     string
+func parseToTwitch(str string) []byte {
+	return []byte(fmt.Sprintf("%s\r\n", str))
 }
 
-var oauth2Config *clientcredentials.Config
-
-const base_url = "https://id.twitch.tv/oauth2/authorize?"
-
-func getCredentials() Credentials {
-	err := godotenv.Load()
-
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
-	cred := Credentials{
-		ClientID:     os.Getenv("CLIENT_ID"),
-		ClientSecret: os.Getenv("CLIENT_SECRET"),
-		TokenURL:     twitch.Endpoint.TokenURL,
-	}
-
-	return cred
+func sendMessage(channel string, message string) []byte {
+	msg := fmt.Sprintf("PRIVMSG #%s :%s", channel, message)
+	return parseToTwitch(msg)
 }
 
-func getAccessToken(cred Credentials) string {
-	oauth2Config = &clientcredentials.Config{
-		ClientID:     cred.ClientID,
-		ClientSecret: cred.ClientSecret,
-		TokenURL:     cred.TokenURL,
+func execMessage(conn net.Conn, message parser.Message) {
+	fmt.Println(message)
+	//	fmt.Println(message.Username == "Twitch")
+	if message.Command == "PONG" {
+		conn.Write(parseToTwitch(message.Content))
 	}
-
-	token, err := oauth2Config.Token(context.Background())
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return token.AccessToken
-}
-
-func clientCred() {
-	cred := getCredentials()
-	token := getAccessToken(cred)
-	fmt.Println(token)
 }
 
 func main() {
-	clientCred()
+	conn := connection.Connect()
+	connection.Logon(conn)
+	tp := textproto.NewReader(bufio.NewReader(conn))
+	for {
+		status, err := tp.ReadLine()
+		if err != nil {
+			panic(err)
+		}
+		//	fmt.Println(status)
+		message := parser.Parse(status)
+		execMessage(conn, message)
+		//	fmt.Println(message)
+	}
+	connection.Disconnect(conn)
 }
