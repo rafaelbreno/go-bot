23209089adbcea7c6cfff4b850a805764625bc53
Rafael Breno <rafael_breno@outlook.com>
2020-12-21 17:44:30 -0300

    Add package to iterate IRC messages

--- cmd/irc/irc.go
+++ cmd/irc/irc.go
@@ -1,89 +1,34 @@
 package irc
 
 import (
-	"fmt"
-	"go-bot/cmd/app_error"
-	"go-bot/cmd/env"
+	"go-bot/app/message"
+
+	"bufio"
+	"log"
 	"net"
+	"net/textproto"
 )
 
-type Conn struct {
-	Cred Credentials
+type IRC struct {
 	Conn net.Conn
-	Env  env.Env
 }
 
-type Credentials struct {
-	ConnURL      string
-	ConnPort     string
-	ConnProtocol string
+func Listen(conn net.Conn) {
+	irc := IRC{conn}
 
-	BotUsername   string
-	BotPassword   string
-	BotOAuthToken string
-
-	TwitchChannel string
+	irc.Read()
 }
 
-func GetConn() *Conn {
-	conn := Conn{
-		Env: env.Env{},
+func (i *IRC) Read() {
+	tp := textproto.NewReader(bufio.NewReader(i.Conn))
+
+	for {
+		msg, err := tp.ReadLine()
+
+		if err != nil {
+			log.Panicln(err.Error())
+		}
+
+		go message.ReadAndParse(msg)
 	}
-
-	conn.
-		setCredentials().
-		setConn().
-		connectIRC()
-
-	return &conn
-}
-
-func (c *Conn) setCredentials() *Conn {
-	cred := Credentials{
-		ConnURL:       "irc.chat.twitch.tv",
-		ConnPort:      "6667",
-		ConnProtocol:  "tcp",
-		BotUsername:   c.Env.Getenv("BOT_USERNAME"),
-		BotOAuthToken: c.Env.Getenv("OAUTH_TOKEN"),
-		TwitchChannel: c.Env.Getenv("CHANNEL_NAME"),
-	}
-	c.Cred = cred
-
-	return c
-}
-
-//
-func (c *Conn) setConn() *Conn {
-	conn, e := net.
-		Dial(c.Cred.ConnProtocol,
-			fmt.Sprintf("%s:%s", c.Cred.ConnURL, c.Cred.ConnPort))
-
-	if e != nil {
-		app_error.NewError(e.Error(), "cmd/irc/irc.go:setConn")
-	}
-
-	c.Conn = conn
-
-	return c
-}
-
-/* Setting connection with Channel's Chat IRC */
-func (c *Conn) connectIRC() {
-	c.Write(fmt.Sprintf("PASS %s", c.Cred.BotOAuthToken))
-	c.Write(fmt.Sprintf("NICK %s", c.Cred.BotUsername))
-	c.Write(fmt.Sprintf("JOIN #%s", c.Cred.TwitchChannel))
-}
-
-func (c *Conn) Disconnect() {
-	if e := c.Conn.Close(); e != nil {
-		app_error.NewError(e.Error(), "cmd/irc/irc.go:Disconnect")
-	}
-}
-
-/* Helper to write IRC messages
- * following the default structure:
- * MSG \r\n
-**/
-func (c *Conn) Write(msg string) {
-	c.Conn.Write([]byte(fmt.Sprintf("%s\r\n", msg)))
 }
