a2925b36955b9662ac5c50d7127d11801655be0e
Rafael Breno <rafael_breno@outlook.com>
2020-12-29 23:31:48 -0300

    Implement method to parse command messages

--- app/command/command.go
+++ app/command/command.go
@@ -9,7 +9,30 @@ import (
 	"log"
 	"net"
 	"os"
+	"regexp"
 	"strings"
+)
+
+const (
+	COMMAND_VAR_REGEXP = `` +
+		// . Any character
+		// ? Any lenght
+		`.?` +
+
+		// Need to start with '__'
+		`__` +
+
+		/* Need to have at least one of:
+		 * a-z Character
+		 * A-Z Character
+		 * @ Character
+		 * ( Character
+		 * ) Character
+		**/
+		`[a-zA-z@\(\)]{1,}` +
+
+		// Need to end with '__'
+		`__`
 )
 
 type Message struct {
@@ -32,6 +55,27 @@ type Commands struct {
 type Command struct {
 	Trigger  string `json:"trigger"`
 	Response string `json:"response"`
+}
+
+type Parser struct {
+	// __Sender@Username__ has __Sender@Coins(int)__ coins
+	RawString string
+
+	// [__Sender@Username__ __Sender@Coins(int)__ ]
+	Keys []Key
+
+	// @rafiusky has 200 coins
+	PreparedString string
+
+	MessageData Message
+}
+
+type Key struct {
+	// __Sender@Username__
+	RawKey string
+
+	// rafiusky
+	PreparedKey string
 }
 
 var commands Commands
@@ -85,11 +129,75 @@ func (m Message) sendCommand(r string) {
 }
 
 func (c Command) prepareResponse(m Message) string {
-	resp := c.Response
+	p := Parser{
+		MessageData: m,
+		RawString:   c.Response,
+	}
 
-	resp = strings.ReplaceAll(resp, "__USER__", "@"+m.Username)
+	p.Parse()
 
-	return resp
+	log.Println("prepareResponse", p.PreparedString)
+
+	//resp := c.Response
+
+	//resp = strings.ReplaceAll(resp, "__USER__", "@"+m.Username)
+
+	return p.PreparedString
+}
+
+func (p *Parser) Parse() {
+	p.
+		setKeys().
+		prepareKeys()
+}
+
+func (p *Parser) setKeys() *Parser {
+	re := regexp.MustCompile(COMMAND_VAR_REGEXP)
+	foundKeys := re.FindAllString(p.RawString, -1)
+
+	for _, value := range foundKeys {
+		p.Keys = append(p.Keys, Key{
+			RawKey: value,
+		})
+	}
+
+	return p
+}
+
+func (p *Parser) prepareKeys() *Parser {
+	p.PreparedString = p.RawString
+
+	for _, value := range p.Keys {
+		var err error
+		// Sender@Username
+		trimmed := strings.Trim(value.RawKey, " ")
+		trimmed = strings.Trim(trimmed, "__")
+
+		// [Sender Username]
+		parts := strings.Split(trimmed, "@")
+
+		value.PreparedKey, err = p.getValue(parts)
+
+		if err != nil {
+			panic(err)
+		}
+
+		p.PreparedString = strings.ReplaceAll(p.PreparedString, value.RawKey, value.PreparedKey)
+	}
+	return p
+}
+
+func (p Parser) getValue(parts []string) (string, error) {
+	switch parts[0] {
+	// Who sent the message
+	case "Target":
+		fallthrough
+	case "Sender":
+		// User struct tags
+		return user.GetByTag(p.MessageData.Username, parts[1])
+	default:
+		return "", fmt.Errorf("Key %s not found!", parts[0])
+	}
 }
 
 func SaveCommands() {
