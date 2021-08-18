7e13475c401645cb5c0189e7a8cd6ebf330d3dda
Rafael <rafael_breno@outlook.com>
2021-08-18 19:12:31 -0300

    feat: Add flag to allow graceful shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/joho/godotenv"
 )
 
 func main() {
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
 	// Loading .env file
 	godotenv.Load()
+
+	<-stop
 }
