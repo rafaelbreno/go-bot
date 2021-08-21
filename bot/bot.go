3d666f72b6c8a0fd8dd6ec970585fbb7b3081d97
Rafael <rafael_breno@outlook.com>
2021-08-21 13:04:23 -0300

    feat: Implement msg via channels

--- bot/bot.go
+++ bot/bot.go
@@ -1,6 +1,8 @@
 package bot
 
 import (
+	"fmt"
+
 	"github.com/rafaelbreno/go-bot/command"
 	"github.com/rafaelbreno/go-bot/conn"
 	"github.com/rafaelbreno/go-bot/internal"
@@ -15,13 +17,29 @@ type Bootstrap struct {
 }
 
 var b *Bootstrap
+var ch chan string
 
 // Start ignites the bot
 func Start(ctx *internal.Context, irc *conn.IRC) {
+	ch = make(chan string, 1)
+
 	b = &Bootstrap{
 		Ctx: ctx,
 		IRC: irc,
 	}
+	b.Ctx.Logger.Info("Start bot")
 
-	go b.IRC.Listen()
+	go b.IRC.Listen(ch)
+	b.ParseMsg()
+}
+
+// ParseMsg aa
+func (b *Bootstrap) ParseMsg() {
+	b.Ctx.Logger.Info("Start parser")
+	for {
+		select {
+		case msg := <-ch:
+			fmt.Println(msg)
+		}
+	}
 }
--- conn/irc.go
+++ conn/irc.go
@@ -22,12 +22,40 @@ type IRC struct {
 	Conn net.Conn
 	Ctx  *internal.Context
 	TP   *textproto.Reader
+	Msg  chan string
 }
 
 // NewIRC returns a IRC
 // struct pointer with
 // configured connection
 func NewIRC(ctx *internal.Context) (*IRC, error) {
+	return &IRC{
+		Ctx: ctx,
+		Msg: make(chan string, 1),
+	}, nil
+}
+
+// Listen start listen IRC channel,
+// and if it disconnects, it will try
+// to reconnect 3 times
+func (i *IRC) Listen(ch chan string) {
+	i.connect()
+
+	go func() {
+		for {
+			msg, err := i.TP.ReadLine()
+			if err != nil {
+				close(i.Msg)
+				i.Ctx.Logger.Error(err.Error())
+				i.connect()
+				continue
+			}
+			ch <- msg
+		}
+	}()
+}
+
+func (i *IRC) connect() {
 	connStr := fmt.Sprintf(ircConnURL, os.Getenv("IRC_URL"), os.Getenv("IRC_PORT"))
 
 	fmt.Println(connStr)
@@ -38,44 +66,21 @@ func NewIRC(ctx *internal.Context) (*IRC, error) {
 	for tries := 1; tries <= 3; tries++ {
 		c, err = net.Dial("tcp", connStr)
 		if err == nil {
+			i.Conn = c
 			break
 		}
 		errMsg := fmt.Sprintf("Error %s. Try number %d!", err.Error(), tries)
-		ctx.Logger.Error(errMsg)
+		i.Ctx.Logger.Error(errMsg)
 		c.Close()
 		time.Sleep(2 * time.Second)
 	}
 
-	return &IRC{
-		Conn: c,
-		Ctx:  ctx,
-	}, err
-}
-
-// Listen start listen IRC channel,
-// and if it disconnects, it will try
-// to reconnect 3 times
-func (i *IRC) Listen() {
-	i.connect()
-
-	go func() {
-		for {
-			msg, err := i.TP.ReadLine()
-			if err != nil {
-				i.Ctx.Logger.Error(err.Error())
-				i.connect()
-			}
-			fmt.Println(msg)
-		}
-	}()
-}
-
-func (i *IRC) connect() {
 	fmt.Fprintf(i.Conn, "PASS %s\r\n", os.Getenv("BOT_OAUTH_TOKEN"))
 	fmt.Fprintf(i.Conn, "NICK %s\r\n", os.Getenv("BOT_USERNAME"))
 	fmt.Fprintf(i.Conn, "JOIN #%s\r\n", os.Getenv("CHANNEL_NAME"))
 
 	i.TP = textproto.NewReader(bufio.NewReader(i.Conn))
+	i.Msg = make(chan string, 1)
 }
 
 // Close ends IRC connection
