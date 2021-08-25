7400817a55a8c2299d9b292e4232b3ba914efe28
Rafael <rafael_breno@outlook.com>
2021-08-25 20:34:25 -0300

    feat: Implement multiple connections

--- internal/context.go
+++ internal/context.go
@@ -9,8 +9,27 @@ import (
 type Context struct {
 	// Logger access a logging package
 	// to log all action inside the bot
-	Logger     *zap.Logger
-	Channels   []string
-	OAuthToken string
-	BotName    string
+	Logger      *zap.Logger
+	ChannelName string
+	OAuthToken  string
+	BotName     string
+}
+
+// WriteContexts returns multiples contexts,
+// each for one different channels
+func WriteContexts(l *zap.Logger, authToken, botName string, channels []string) map[string]*Context {
+	var chs map[string]*Context
+
+	for _, channel := range channels {
+		if _, ok := chs[channel]; !ok {
+			chs[channel] = &Context{
+				Logger:      l,
+				ChannelName: channel,
+				OAuthToken:  authToken,
+				BotName:     botName,
+			}
+		}
+	}
+
+	return chs
 }
