cf7528488410420b2a75aba1d9e95d1c5902c6ad
Rafael <rafael_breno@outlook.com>
2021-08-26 23:48:17 -0300

    feat: Define server

--- services/api/cmd/main.go
+++ services/api/cmd/main.go
@@ -6,15 +6,22 @@ import (
 	"os/signal"
 	"syscall"
 
+	"github.com/joho/godotenv"
 	"github.com/rafaelbreno/go-bot/api/internal"
+	"github.com/rafaelbreno/go-bot/api/server"
 	"go.uber.org/zap"
 )
 
 var ctx *internal.Context
+var sv *server.Server
 
 func init() {
-	l, err := zap.NewProduction()
+	if err := godotenv.Load(); err != nil {
+		fmt.Println(err.Error())
+		os.Exit(0)
+	}
 
+	l, err := zap.NewProduction()
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(0)
@@ -22,6 +29,11 @@ func init() {
 
 	ctx = &internal.Context{
 		Logger: l,
+	}
+
+	sv = &server.Server{
+		Ctx:  ctx,
+		Port: os.Getenv("APP_PORT"),
 	}
 }
 
@@ -31,6 +43,9 @@ func main() {
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 
+	go sv.ListenAndServe()
+
+	defer sv.Close()
 	<-stop
 
 	ctx.Logger.Info("Gracefully terminating...")
---
+++ services/api/server/server.go
@@ -0,0 +1,43 @@
+package server
+
+import (
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"github.com/rafaelbreno/go-bot/api/internal"
+	"os"
+)
+
+type Server struct {
+	App  *fiber.App
+	Ctx  *internal.Context
+	Port string
+}
+
+func (s *Server) ListenAndServe() {
+	s.App = fiber.New()
+
+	if s.Port == "" {
+		s.Ctx.Logger.Error("Empty port")
+		os.Exit(0)
+	}
+
+	s.App.Get("/test", func (c *fiber.Ctx) error {
+		s.Ctx.Logger.Info("GET /test")
+		return c.JSON(map[string]string{
+			"message": "ok",
+		})
+	})
+
+	s.Ctx.Logger.Info(fmt.Sprintf("Starting server, listening port: %s", s.Port))
+	if err := s.App.Listen(s.Port); err != nil {
+		s.Ctx.Logger.Error(err.Error())
+	}
+}
+
+func (s *Server) Close() {
+	s.Ctx.Logger.Info("Shutting down server...")
+	if err := s.App.Shutdown(); err != nil {
+		s.Ctx.Logger.Error(err.Error())
+	}
+	s.Ctx.Logger.Info("Server shut down!")
+}
