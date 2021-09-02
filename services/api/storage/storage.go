a63eb93925abcbc5448b95cb7cba813367d8685e
Rafael <rafael_breno@outlook.com>
2021-09-02 20:34:24 -0300

    fix: KafkaClient name

--- services/api/storage/storage.go
+++ services/api/storage/storage.go
@@ -8,17 +8,17 @@ import (
 // this API and any resource of storage
 // e.g(Postgres, S3, etc.).
 type Storage struct {
-	SQL  *DB
-	Prod *Prod
-	Ctx  *internal.Context
+	SQL         *DB
+	KafkaClient *KafkaClient
+	Ctx         *internal.Context
 }
 
 // NewStorage return a storage manager
 // for database and in-memory
 func NewStorage(ctx *internal.Context) *Storage {
 	return &Storage{
-		Ctx:  ctx,
-		SQL:  newDB(ctx),
-		Prod: newProd(ctx),
+		Ctx:         ctx,
+		SQL:         newDB(ctx),
+		KafkaClient: newKafkaClient(ctx),
 	}
 }
