a16b17d6b906136375da0484881e7bcfbc353abd
Rafael <rafael_breno@outlook.com>
2021-09-06 20:10:54 -0300

    feat: Add Kafka connection with Consumer

--- services/queue-mgr/src/main.rs
+++ services/queue-mgr/src/main.rs
@@ -1,6 +1,9 @@
 extern crate dotenv;
 
 use dotenv::dotenv;
+use kafka::client::{FetchOffset, GroupOffsetStorage};
+use kafka::consumer::Consumer;
+use kafka::error::Error as KafkaError;
 use std::env;
 use std::string::String;
 use redis;
@@ -29,6 +32,33 @@ fn redis_connect() -> redis::Connection {
         .expect("unable to connect")
 }
 
+fn start_consumer(topic: String, brokers: Vec<String>) -> Result<(), KafkaError> {
+    let mut con = Consumer::from_hosts(brokers)
+        .with_topic(topic)
+        .with_fallback_offset(FetchOffset::Earliest)
+        .with_offset_storage(GroupOffsetStorage::Kafka)
+        .create()?;
+
+    loop {
+        let mss = con.poll()?;
+
+        if mss.is_empty() {
+            println!("No messages")
+        }
+
+        for ms in mss.iter(){
+            for m in ms.messages(){
+                println!("{}:{}@{}: {:?} - {:?}", ms.topic(), ms.partition(), m.offset, m.key, m.value);
+            }
+
+        }
+    }
+}
+
+//KAFKA_URL=localhost
+//KAFKA_PORT=9092
+//KAFKA_TOPIC=commandss
+
 
 fn main() {
     dotenv().ok();
@@ -47,4 +77,25 @@ fn main() {
         .expect("'key' not found");
 
     println!("{}", found);
+
+    let kafka_url = match env::var("KAFKA_URL") {
+        Ok(val) => val,
+        Err(_) => format!("{}", "localhost"),
+    };
+
+    let kafka_port = match env::var("KAFKA_PORT") {
+        Ok(val) => val,
+        Err(_) => format!("{}", "localhost"),
+    };
+    
+    let kafka_topic = match env::var("KAFKA_TOPIC") {
+        Ok(val) => val,
+        Err(_) => format!("{}", "localhost"),
+    };
+
+    let kafka_conn_url = format!("{:}:{:}", kafka_url, kafka_port);
+
+    if let Err(e) = start_consumer(kafka_topic, vec![kafka_conn_url]) {
+        println!("{:}", e)
+    }
 }
