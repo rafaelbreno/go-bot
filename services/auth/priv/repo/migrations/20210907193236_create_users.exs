322926741043b52765ac6358b24c55096b7f7c27
Rafael <rafael_breno@outlook.com>
2021-09-09 22:16:12 -0300

    feat: Implement column sizes and add unique index

--- services/auth/priv/repo/migrations/20210907193236_create_users.exs
+++ services/auth/priv/repo/migrations/20210907193236_create_users.exs
@@ -4,11 +4,14 @@ defmodule Users.Repo.Migrations.CreateUsers do
   def up do
     create table("users", primary_key: false) do
       add :id,        :string, default: "gen_random_uuid()"
-      add :username,  :string
-      add :password,  :string
+      add :username,  :string, size: 16
+      add :password,  :string, size: 64
 
       timestamps()
+
     end
+    create unique_index(:users, [:id])
+    create unique_index(:users, [:username])
   end
 
   def down do
