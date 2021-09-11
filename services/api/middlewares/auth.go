20bc143be73760395b8af49c5e0f49ad5edfdba1
Rafael <rafael_breno@outlook.com>
2021-09-11 19:16:20 -0300

    feat: Add JSON Decoder into Auth's middleware

--- services/api/middlewares/auth.go
+++ services/api/middlewares/auth.go
@@ -1,21 +1,25 @@
 package middlewares
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/rafaelbreno/go-bot/api/config"
 )
 
-type UserAPI struct {
+type userAPI struct {
+	ID string `json:"id"`
 }
 
 // CheckAuth check if user is authenticated
 func CheckAuth(ctx *fiber.Ctx) error {
 	appCtx := config.Ctx
 
-	b := ctx.Request().Header.Peek("Authorization")
+	b := ctx.Get("Authorization")
 	bearer := string(b)
 
 	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("http://%s:%s%s", appCtx.Env["AUTH_URL"], appCtx.Env["AUTH_PORT"], appCtx.Env["AUTH_ENDPOINT"]), nil)
@@ -50,6 +54,24 @@ func CheckAuth(ctx *fiber.Ctx) error {
 				"error": "unauthorized",
 			})
 	}
+
+	resBody := res.Body
+
+	resBytes, _ := ioutil.ReadAll(resBody)
+
+	var v userAPI
+
+	if err := json.Unmarshal(resBytes, &v); err != nil {
+		return ctx.
+			Status(http.StatusBadRequest).
+			JSON(fiber.Map{
+				"error": err.Error(),
+			})
+	}
+
+	userCtx := context.WithValue(ctx.UserContext(), "user_id", v.ID)
+
+	ctx.SetUserContext(userCtx)
 
 	return ctx.Next()
 }
