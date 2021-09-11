432f483265cb8c15afc7b0958484fcc1ebfcc57c
Rafael <rafael_breno@outlook.com>
2021-09-11 16:18:38 -0300

    feat: Auth env variables

--- services/api/config/config.go
+++ services/api/config/config.go
@@ -45,6 +45,8 @@ func init() {
 			"KAFKA_URL":      os.Getenv("KAFKA_URL"),
 			"KAFKA_PORT":     os.Getenv("KAFKA_PORT"),
 			"KAFKA_TOPIC":    os.Getenv("KAFKA_TOPIC"),
+			"AUTH_URL":       os.Getenv("AUTH_URL"),
+			"AUTH_PORT":      os.Getenv("AUTH_PORT"),
 			"API_PORT":       getPort(),
 		},
 	}
