be49b5fa4943221906376940c968f82f3975cb93
Rafael <rafael_breno@outlook.com>
2021-09-11 15:17:03 -0300

    feat: Create UserController

---
+++ services/auth/app/Http/Controllers/UserController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UserRequest;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param UserRequest $request
+     * @return JsonResponse
+     */
+    public function store(UserRequest $request): JsonResponse
+    {
+        return response()->json(
+            User::create($request->validated())
+        , 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
+}
