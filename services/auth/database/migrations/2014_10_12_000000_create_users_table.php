539cd597e31b038faf247ac7ac24638741611c39
Rafael <rafael_breno@outlook.com>
2021-09-11 15:16:03 -0300

    feat: Implement uuid column into users table

--- services/auth/database/migrations/2014_10_12_000000_create_users_table.php
+++ services/auth/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id');
             $table->string('username', 16)->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
