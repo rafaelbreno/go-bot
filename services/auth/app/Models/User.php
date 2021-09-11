642379bb2bdd97c858dd99e8cc208ee0c47e7688
Rafael <rafael_breno@outlook.com>
2021-09-11 15:16:55 -0300

    feat: Implement UserRequest with Model's validation rules

--- services/auth/app/Models/User.php
+++ services/auth/app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Traits\Uuid;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -11,6 +12,12 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable, Uuid;
+
+    const VALIDATION_RULES = [
+        'username' => ['unique:users', 'required', 'max:16', 'min:10'],
+        'password' => ['required', 'confirmed'],
+        'password_confirmation' => ['required'],
+    ];
 
     /**
      * The attributes that are mass assignable.
