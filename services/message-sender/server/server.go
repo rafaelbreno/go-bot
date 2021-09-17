7387ddcb071277a820e7782c9fa049c5df7679ee
Rafael <rafael_breno@outlook.com>
2021-09-16 21:48:07 -0300

    feat: Add SendMessage function

--- services/message-sender/server/server.go
+++ services/message-sender/server/server.go
@@ -17,6 +17,13 @@ type Server struct {
 
 // SendMessage receives a message body to treat
 // and send to a channel's chat
-func (s *Server) SendMessage(ctx context.Context, msg *proto.MessageRequest) *proto.Empty {
-	return s.Repo.SendMessage(msg)
+func (s *Server) SendMessage(ctx context.Context, msg *proto.MessageRequest) (*proto.Empty, error) {
+	return s.Repo.SendMessage(msg), nil
+}
+
+func NewServer(ctx *internal.Context) *Server {
+	return &Server{
+		Ctx:  ctx,
+		Repo: repository.NewMessageRepo(ctx),
+	}
 }
