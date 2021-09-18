2eef48a17d4f1e850d7b83fabaccf2677ab88de5
Rafael <rafael_breno@outlook.com>
2021-09-18 20:55:40 -0300

    feat: Add message

--- services/message-reader/message/message.go
+++ services/message-reader/message/message.go
@@ -1,7 +1,35 @@
 package message
 
+import (
+	"regexp"
+
+	"github.com/rafaelbreno/go-bot/services/message-reader/command"
+	"github.com/rafaelbreno/go-bot/services/message-reader/internal"
+	"github.com/rafaelbreno/go-bot/services/message-reader/storage"
+)
+
 type Message struct {
 	Channel string
 	SentBy  string
 	Value   string
+}
+
+var (
+	triggerRegex = regexp.MustCompile(`![a-zA-Z0-9]{1,}`)
+)
+
+var (
+	nilCommand = command.Command{}
+)
+
+func (m *Message) Send(ctx *internal.Context, st *storage.Storage) {
+	trigger := triggerRegex.FindString(m.Value)
+
+	cmd := storage.GetCommand(m.Channel, trigger, *st)
+
+	if cmd == nilCommand {
+		return
+	}
+
+	ans := cmd.Parse()
 }
