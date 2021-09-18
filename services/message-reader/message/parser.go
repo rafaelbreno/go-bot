39de4225d7e6ba19b9c781b8d8ff56138b725405
Rafael <rafael_breno@outlook.com>
2021-09-18 20:22:30 -0300

    feat: Add IRC message parser

---
+++ services/message-reader/message/parser.go
@@ -0,0 +1,65 @@
+package message
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+
+	"github.com/rafaelbreno/go-bot/services/message-reader/conn"
+	"github.com/rafaelbreno/go-bot/services/message-reader/internal"
+)
+
+var (
+	channelNameRegex = regexp.MustCompile(`#[a-zA-Z0-9\_]{1,}`)
+)
+
+type Parser struct {
+	Ctx *internal.Context
+	IRC *conn.IRC
+}
+
+func (p *Parser) Pong() {
+	fmt.Fprint(p.IRC.Conn, fmt.Sprint("PONG\r\n"))
+}
+
+var (
+	channelRegex = regexp.MustCompile(`#[a-zA-Z0-9\_]{1,}`)
+)
+
+// :ricardinst!ricardinst@ricardinst.tmi.twitch.tv PRIVMSG #rafiusky :e
+func (p *Parser) Send(msg string) {
+	fmt.Println(msg)
+
+	if msg[0:4] == "PING" {
+		p.Pong()
+		return
+	}
+
+	byteMsg := []byte(msg)
+	var sentBy []byte
+
+	for _, b := range byteMsg {
+		if b == byte('!') {
+			break
+		}
+		sentBy = append(sentBy, b)
+	}
+
+	channel := channelNameRegex.FindString(msg)
+
+	if channel == "" {
+		return
+	}
+
+	val := strings.SplitN(msg, ":", 3)
+
+	if len(val) <= 3 {
+		return
+	}
+
+	_ = Message{
+		SentBy:  string(sentBy),
+		Channel: channel[1:],
+		Value:   val[2],
+	}
+}
