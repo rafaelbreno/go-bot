7f2bc58394e22360139cd59099263b6ef0a00738
Rafael <rafael_breno@outlook.com>
2021-09-18 16:43:43 -0300

    feat: Add IRC related functions

--- services/message-reader/reader/reader.go
+++ services/message-reader/reader/reader.go
@@ -1,32 +1,69 @@
 package reader
 
 import (
+	"fmt"
+
 	"github.com/rafaelbreno/go-bot/services/message-reader/conn"
+	"github.com/rafaelbreno/go-bot/services/message-reader/helpers"
 	"github.com/rafaelbreno/go-bot/services/message-reader/internal"
 	"github.com/rafaelbreno/go-bot/services/message-reader/storage"
 )
 
 type Reader struct {
-	Storage storage.Storage
-	IRC     *conn.IRC
-	Ctx     *internal.Context
+	Channels []string
+	Storage  storage.Storage
+	IRC      *conn.IRC
+	Ctx      *internal.Context
 }
 
 func NewReader(ctx *internal.Context, st storage.Storage) *Reader {
 	irc := conn.NewIRC(ctx)
 
 	return &Reader{
-		Storage: st,
-		Ctx:     ctx,
-		IRC:     irc,
+		Channels: []string{},
+		Storage:  st,
+		Ctx:      ctx,
+		IRC:      irc,
 	}
 }
 
 func (r *Reader) Start() {
+	r.joinRedis()
 	for {
 		select {
 		case msg := <-r.IRC.Msg:
 			r.Ctx.Logger.Info(msg)
 		}
+	}
+}
+
+func (r *Reader) joinRedis() {
+	for _, c := range storage.GetChannels(r.Storage) {
+		r.Channels = append(r.Channels, c)
+		r.JoinChat(c)
+	}
+}
+
+func (r *Reader) CheckChannel(name string) {
+	if !helpers.FindInSliceStr(r.Channels, name) {
+		r.Channels = append(r.Channels, name)
+		r.JoinChat(name)
+	}
+}
+
+func (r *Reader) JoinChat(name string) {
+	r.write(fmt.Sprintf("JOIN #%s", name))
+}
+
+func (r *Reader) partChat(name string) {
+	r.write(fmt.Sprintf("PART #%s", name))
+	r.Channels = helpers.RemoveElementStr(r.Channels, name)
+}
+
+func (r *Reader) write(msg string) {
+	r.Ctx.Logger.Info("Sending Message")
+	_, err := r.IRC.Conn.Write([]byte(fmt.Sprintf("%s\r\n", msg)))
+	if err != nil {
+		r.Ctx.Logger.Error(err.Error())
 	}
 }
