627034d7014392af57363065443c7284442d655a
Rafael <rafael_breno@outlook.com>
2021-09-20 23:39:00 -0300

    feat: Add gRPC client into server

--- services/message-reader/server/server.go
+++ services/message-reader/server/server.go
@@ -2,32 +2,23 @@ package server
 
 import (
 	"context"
-	"fmt"
+	"time"
 
+	"github.com/rafaelbreno/go-bot/services/message-reader/client"
 	"github.com/rafaelbreno/go-bot/services/message-reader/internal"
 	"github.com/rafaelbreno/go-bot/services/message-reader/proto"
 	"google.golang.org/grpc"
 )
 
 type Server struct {
-	Msg    chan *proto.MessageRequest
-	Client proto.SenderClient
+	Client *client.Client
 	Ctx    *internal.Context
 }
 
 func NewServer(ctx *internal.Context) *Server {
-	addr := "localhost:5004"
-
-	conn, err := grpc.Dial(addr, grpc.WithInsecure())
-
-	if err != nil {
-		ctx.Logger.Panic(err.Error())
-	}
-
-	client := proto.NewSenderClient(conn)
+	client := client.NewClient(ctx)
 
 	return &Server{
-		Msg:    make(chan *proto.MessageRequest),
 		Client: client,
 		Ctx:    ctx,
 	}
@@ -36,12 +27,35 @@ func NewServer(ctx *internal.Context) *Server {
 func (s *Server) Start() {
 	for {
 		select {
-		case msg := <-s.Msg:
-			if _, err := s.Client.SendMessage(context.Background(), msg, nil); err != nil {
-				s.Ctx.Logger.Panic(err.Error())
-			}
-			fmt.Println(msg.Channel)
-			fmt.Println(msg.Msg)
+		case msg := <-*s.Ctx.Msg:
+			SendMessage(msg)
+			//fmt.Println(msg.Channel)
+			//fmt.Println(msg.Msg)
 		}
 	}
+}
+
+func SendMessage(msg *proto.MessageRequest) {
+	conn, err := grpc.Dial("localhost:5004", grpc.WithInsecure())
+
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	client := proto.NewSenderClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*4)
+
+	defer cancel()
+
+	_, err = client.SendMessage(ctx, msg)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (s *Server) Close() {
+	s.Client.Conn.Close()
 }
