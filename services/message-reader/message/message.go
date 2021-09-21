8bf5a225700c4e1729ae6aca2fe524f23dc05814
Rafael <rafael_breno@outlook.com>
2021-09-20 23:38:14 -0300

    feat: Routing messageRequest to context

--- services/message-reader/message/message.go
+++ services/message-reader/message/message.go
@@ -3,8 +3,8 @@ package message
 import (
 	"regexp"
 
-	"github.com/rafaelbreno/go-bot/services/message-reader/command"
 	"github.com/rafaelbreno/go-bot/services/message-reader/internal"
+	"github.com/rafaelbreno/go-bot/services/message-reader/proto"
 	"github.com/rafaelbreno/go-bot/services/message-reader/storage"
 )
 
@@ -18,10 +18,6 @@ var (
 	triggerRegex = regexp.MustCompile(`![a-zA-Z0-9]{1,}`)
 )
 
-var (
-	nilCommand = command.Command{}
-)
-
 func (m *Message) Send(ctx *internal.Context, st *storage.Storage) {
 	trigger := triggerRegex.FindString(m.Value)
 
@@ -31,9 +27,8 @@ func (m *Message) Send(ctx *internal.Context, st *storage.Storage) {
 
 	cmd := storage.GetCommand(m.Channel, trigger, *st)
 
-	if nilCommand == cmd {
-		return
+	*ctx.Msg <- &proto.MessageRequest{
+		Channel: m.Channel,
+		Msg:     cmd.Parse(),
 	}
-
-	ans := cmd.Parse()
 }
