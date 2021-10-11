8bec71e54d0abc551701c0c150d58adefa9fdfcc
Rafael <rafael_breno@outlook.com>
2021-10-11 20:20:46 -0300

    feat: Add validation into create method

--- services/auth/repository/auth.go
+++ services/auth/repository/auth.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"fmt"
 
+	"github.com/go-pg/pg/v10"
 	"github.com/rafaelbreno/go-bot/auth/internal"
 	"github.com/rafaelbreno/go-bot/auth/proto"
 	"github.com/rafaelbreno/go-bot/auth/storage"
@@ -21,11 +22,31 @@ type AuthRepoCtx struct {
 	Storage *storage.Storage
 }
 
+func NewAuthRepo(c *internal.Common, sto *storage.Storage) *AuthRepoCtx {
+	a := AuthRepoCtx{
+		Common:  c,
+		Storage: sto,
+	}
+	return &a
+}
+
 const (
 	fieldsNotMatch = `'%s' and '%s' don't match`
+	emptyField     = `field '%s' can not be empty`
+	wrongPassord   = `wrong password`
 )
 
 func (a *AuthRepoCtx) Create(req *proto.CreateRequest) *proto.CreateResponse {
+	if req.Username == "" {
+		return &proto.CreateResponse{
+			Error: fmt.Sprintf(emptyField, "username"),
+		}
+	}
+	if req.Password == "" {
+		return &proto.CreateResponse{
+			Error: fmt.Sprintf(emptyField, "password"),
+		}
+	}
 	if req.Password != req.PasswordConfirmation {
 		errMsg := fmt.Sprintf(fieldsNotMatch, "password", "password_confirmation")
 		return &proto.CreateResponse{
@@ -34,7 +55,7 @@ func (a *AuthRepoCtx) Create(req *proto.CreateRequest) *proto.CreateResponse {
 		}
 	}
 
-	encPW, err := bcrypt.GenerateFromPassword([]byte(req.Password), 14)
+	encPW, err := a.EncPassword(req.Password)
 
 	if err != nil {
 		return &proto.CreateResponse{
@@ -47,13 +68,15 @@ func (a *AuthRepoCtx) Create(req *proto.CreateRequest) *proto.CreateResponse {
 		Password: string(encPW),
 	}
 
-	if _, err := a.Storage.Pg.Conn.Model(u).Insert(); err != nil {
+	if _, err := a.Storage.Pg.Conn.Model(&u).Insert(); err != nil {
 		return &proto.CreateResponse{
 			Error: err.Error(),
 		}
 	}
 
-	return &proto.CreateResponse{}
+	return &proto.CreateResponse{
+		Token: "teste",
+	}
 }
 
 func (a *AuthRepoCtx) Login(req *proto.LoginRequest) *proto.LoginResponse {
@@ -64,10 +87,12 @@ func (a *AuthRepoCtx) Check(req *proto.CheckRequest) *proto.CheckResponse {
 	return &proto.CheckResponse{}
 }
 
-func NewAuthRepo(c *internal.Common, sto *storage.Storage) *AuthRepoCtx {
-	a := AuthRepoCtx{
-		Common:  c,
-		Storage: sto,
-	}
-	return &a
+func (a *AuthRepoCtx) EncPassword(pw string) (string, error) {
+	encPW, err := bcrypt.GenerateFromPassword([]byte(pw), 14)
+	return string(encPW), err
+}
+
+func (a *AuthRepoCtx) CheckPassword(encPW, pw string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(encPW), []byte(pw))
+	return err == nil
 }
