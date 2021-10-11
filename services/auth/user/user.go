277b22069d7f7f18f70303c0ec49ea7ed9412d85
Rafael <rafael_breno@outlook.com>
2021-10-11 20:19:31 -0300

    feat: Add more fields options

--- services/auth/user/user.go
+++ services/auth/user/user.go
@@ -3,7 +3,7 @@ package user
 import "github.com/google/uuid"
 
 type User struct {
-	Id       uuid.UUID `pg:"type:uuid,default:gen_random_uuid()"`
-	Username string
+	Id       uuid.UUID `pg:"type:uuid,pk,default:gen_random_uuid()"`
+	Username string    `pg:"username,unique"`
 	Password string
 }
